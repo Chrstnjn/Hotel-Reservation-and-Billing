a91e9060f23102b62d213813bf7271179bbe052f
Adrian-Izar <aidizar026@gmail.com>
2026-09-24 13:09:12 +0800

    Login and Dashboard design

--- HotelReservation.App/Form1.Designer.cs
+++ HotelReservation.App/Form1.Designer.cs
@@ -28,18 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            btnSignin = new Button();
+            label2 = new Label();
+            label1 = new Label();
+            txtPassword = new TextBox();
+            txtLogin = new TextBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(200, 209, 217);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(btnSignin);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(txtPassword);
+            panel1.Controls.Add(txtLogin);
+            panel1.Location = new Point(224, 43);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(475, 514);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.ErrorImage = null;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.InitialImage = (Image)resources.GetObject("pictureBox1.InitialImage");
+            pictureBox1.Location = new Point(63, 29);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(357, 181);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // btnSignin
+            // 
+            btnSignin.BackColor = Color.FromArgb(28, 100, 236);
+            btnSignin.FlatStyle = FlatStyle.Popup;
+            btnSignin.Font = new Font("Constantia", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSignin.ForeColor = SystemColors.ControlLightLight;
+            btnSignin.Location = new Point(163, 436);
+            btnSignin.Name = "btnSignin";
+            btnSignin.Size = new Size(154, 44);
+            btnSignin.TabIndex = 4;
+            btnSignin.Text = "SIGNIN";
+            btnSignin.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Constantia", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(63, 337);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 24);
+            label2.TabIndex = 3;
+            label2.Text = "Password";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Constantia", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(63, 257);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 24);
+            label1.TabIndex = 2;
+            label1.Text = "Login";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Font = new Font("Consolas", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtPassword.Location = new Point(63, 364);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(357, 39);
+            txtPassword.TabIndex = 1;
+            // 
+            // txtLogin
+            // 
+            txtLogin.Font = new Font("Consolas", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtLogin.Location = new Point(63, 284);
+            txtLogin.Name = "txtLogin";
+            txtLogin.Size = new Size(357, 39);
+            txtLogin.TabIndex = 0;
             // 
             // frmLogin
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.FromArgb(45, 69, 115);
+            ClientSize = new Size(914, 600);
+            Controls.Add(panel1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "frmLogin";
             Text = "Login Page";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private TextBox txtLogin;
+        private PictureBox pictureBox1;
+        private Button btnSignin;
+        private Label label2;
+        private Label label1;
+        private TextBox txtPassword;
     }
 }
---
+++ HotelReservation.App/frmReceptionistDashboard.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace HotelReservation.App
+{
+    partial class frmReceptionistDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmReceptionistDashboard));
+            panel2 = new Panel();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            button1 = new Button();
+            btnMayAccount = new Button();
+            btnManagement = new Button();
+            label2 = new Label();
+            btnDashboard = new Button();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(215, 218, 224);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Location = new Point(-6, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(920, 64);
+            panel2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Constantia", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(205, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(276, 28);
+            label1.TabIndex = 3;
+            label1.Text = "Receptionist Dashboard";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.ErrorImage = null;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.InitialImage = (Image)resources.GetObject("pictureBox1.InitialImage");
+            pictureBox1.Location = new Point(3, -2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(196, 66);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(19, 52, 113);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(btnMayAccount);
+            panel1.Controls.Add(btnManagement);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(btnDashboard);
+            panel1.Location = new Point(1, 62);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(192, 540);
+            panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(213, 64, 64);
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Franklin Gothic Book", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ControlLightLight;
+            button1.Location = new Point(52, 501);
+            button1.Name = "button1";
+            button1.Size = new Size(87, 25);
+            button1.TabIndex = 8;
+            button1.Text = "LOGOUT";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // btnMayAccount
+            // 
+            btnMayAccount.BackColor = Color.FromArgb(48, 91, 171);
+            btnMayAccount.FlatStyle = FlatStyle.Popup;
+            btnMayAccount.Font = new Font("Franklin Gothic Book", 7.8F, FontStyle.Bold);
+            btnMayAccount.ForeColor = SystemColors.ControlLightLight;
+            btnMayAccount.Location = new Point(20, 158);
+            btnMayAccount.Name = "btnMayAccount";
+            btnMayAccount.Size = new Size(152, 35);
+            btnMayAccount.TabIndex = 6;
+            btnMayAccount.Text = "MY ACCOUNT";
+            btnMayAccount.UseVisualStyleBackColor = false;
+            // 
+            // btnManagement
+            // 
+            btnManagement.BackColor = Color.FromArgb(48, 91, 171);
+            btnManagement.FlatStyle = FlatStyle.Popup;
+            btnManagement.Font = new Font("Franklin Gothic Book", 7.8F, FontStyle.Bold);
+            btnManagement.ForeColor = SystemColors.ControlLightLight;
+            btnManagement.Location = new Point(20, 107);
+            btnManagement.Name = "btnManagement";
+            btnManagement.Size = new Size(152, 35);
+            btnManagement.TabIndex = 7;
+            btnManagement.Text = "BOOKINGS";
+            btnManagement.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Constantia", 7.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(101, 138, 208);
+            label2.Location = new Point(11, 21);
+            label2.Name = "label2";
+            label2.Size = new Size(74, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Navigation";
+            // 
+            // btnDashboard
+            // 
+            btnDashboard.BackColor = Color.FromArgb(48, 91, 171);
+            btnDashboard.FlatStyle = FlatStyle.Popup;
+            btnDashboard.Font = new Font("Franklin Gothic Book", 7.8F, FontStyle.Bold);
+            btnDashboard.ForeColor = SystemColors.ControlLightLight;
+            btnDashboard.Location = new Point(20, 57);
+            btnDashboard.Name = "btnDashboard";
+            btnDashboard.Size = new Size(152, 35);
+            btnDashboard.TabIndex = 5;
+            btnDashboard.Text = "DASHBOARD";
+            btnDashboard.UseVisualStyleBackColor = false;
+            // 
+            // frmReceptionistDashboard
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(914, 600);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "frmReceptionistDashboard";
+            Text = "frmReceptionistDashboard";
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel2;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private Button button1;
+        private Button btnMayAccount;
+        private Button btnManagement;
+        private Label label2;
+        private Button btnDashboard;
+    }
+}
